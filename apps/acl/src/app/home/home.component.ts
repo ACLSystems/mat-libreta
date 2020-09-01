206e24d89eecbf889552d34d0fd38d2566ebbacc
Arturo Castro Loera <arturocastro@aclsystems.mx>
2020-09-01 18:47:16 -0500

    Modificación Página Inicial de ACL Systems

--- apps/acl/src/app/home/home.component.ts
+++ apps/acl/src/app/home/home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	year: string;
+
+  constructor() {
+		this.year = new Date().getFullYear().toString();
+	}
 
   ngOnInit(): void {
   }
