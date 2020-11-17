32854ab9fe86f67968ce6e9e78c4f6d2f8f6966a
Arturo Castro Loera <arturocastro@aclsystems.mx>
2020-11-17 11:40:27 -0600

    ACL2

--- apps/acl/src/app/pages/freshservice/freshservice.component.ts
+++ apps/acl/src/app/pages/freshservice/freshservice.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mat-libreta-freshservice',
-  templateUrl: './freshservice.component.html',
+  // templateUrl: './freshservice.component.html',
+	templateUrl: './vacio.html',
   styleUrls: ['./freshservice.component.scss']
 })
 export class FreshserviceComponent implements OnInit {
